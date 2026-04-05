6593d84f63c0244d6d12e49ddf6a133e2484c6c3
Veysi yê MALA SAF <veysieken000@gmail.com>
2026-04-05 12:09:25 +0300

    Create knowledge.js

---
+++ knowledge.js
@@ -0,0 +1,49 @@
+const KNOWLEDGE = {
+  rasterast: [
+    "Rasterast'ta tutarlılık filtresini açıkla.",
+    "Rasterast'ı klasik bilimsel yöntemle karşılaştır.",
+    "Rasterast'ın hata eleme mantığını açıkla."
+  ],
+
+  rebun: [
+    "Rebûn yönetim modelini doğa düzeniyle ilişkilendir.",
+    "Rebûn'ü modern devlet yapılarıyla karşılaştır.",
+    "Rebûn modelinde adaletin yerini açıkla."
+  ],
+
+  civilization: [
+    "Newroza Kawa uygarlığının temel ilkelerini açıkla.",
+    "Medeniyetin çöküş ve yeniden inşa mantığını açıkla.",
+    "Bu konunun Zanistarast sentezi içindeki yerini açıkla."
+  ],
+
+  fitrah_ethics: [
+    "Fıtrat ve ahlak sistemini sistematik olarak açıkla.",
+    "Fıtrat ile insan doğası ilişkisini açıkla.",
+    "Ahlakın neden yapısal zorunluluk olduğunu açıkla."
+  ],
+
+  zanistarast: [
+    "Zanistarast bilimsel sentezini katmanlı olarak açıkla.",
+    "Zanistarast'ı modern bilim anlayışıyla karşılaştır.",
+    "Zanistarast'ın medeniyet hedefini açıkla."
+  ],
+
+  time: [
+    "Zamanı Zanistarast bilimsel sentezine göre açıkla.",
+    "Zaman ile varlık ilişkisini açıkla.",
+    "Zamanın medeniyet üzerindeki etkisini açıkla."
+  ],
+
+  ecology: [
+    "Jeolojiyi katmanlı gerçeklik modeline göre açıkla.",
+    "Jeoloji ile varlık tarihi ilişkisini açıkla.",
+    "Jeolojinin insanlık tarihine etkisini açıkla."
+  ],
+
+  philosophy: [
+    "Felsefeyi Zanistarast çerçevesinde yeniden açıkla.",
+    "Bunu klasik felsefeyle karşılaştır.",
+    "Felsefenin ontoloji ve epistemolojiyle bağını açıkla."
+  ]
+};
