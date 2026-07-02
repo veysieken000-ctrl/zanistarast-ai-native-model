25523752bfc67c1d0de0673f2d7309e73617fa65
Veysi yê MALA SAF <veysieken000@gmail.com>
2026-07-02 14:30:09 +0300

    feat(loader): add Lean formalization loader

---
+++ backend/loader/lean_loader.js
@@ -0,0 +1,60 @@
+/**
+ * Lean Loader
+ * ZANISTARAST AI Native Model
+ */
+
+const fs = require("fs");
+const path = require("path");
+
+class LeanLoader {
+
+    constructor() {
+
+        this.files = {};
+
+    }
+
+    load(directory) {
+
+        const files = fs.readdirSync(directory);
+
+        for (const file of files) {
+
+            if (!file.endsWith(".lean"))
+                continue;
+
+            const fullPath =
+                path.join(directory, file);
+
+            this.files[file] =
+                fs.readFileSync(fullPath, "utf8");
+        }
+
+        return this.files;
+    }
+
+    get(fileName) {
+
+        return this.files[fileName];
+
+    }
+
+    getAll() {
+
+        return this.files;
+
+    }
+
+    exists(fileName) {
+
+        return fileName in this.files;
+
+    }
+
+}
+
+module.exports =
+    new LeanLoader();
+
+
+
