3da9b1bb890d51b8d66361162fbdd5fb3b921c45
Veysi yê MALA SAF <veysieken000@gmail.com>
2026-09-24 11:00:35 +0300

    pwa: make install resilient to optional asset failures

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "zanistarast-core-v2";
+const CACHE_NAME = "zanistarast-core-v3";
 
 const CORE_ASSETS = [
   "./",
@@ -14,8 +14,13 @@ const CORE_ASSETS = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+    caches.open(CACHE_NAME).then(async cache => {
+      await Promise.allSettled(
+        CORE_ASSETS.map(asset => cache.add(asset))
+      );
+    })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
