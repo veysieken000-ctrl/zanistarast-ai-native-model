0c96cb7fe98d4721682e9f0e67915d71f180b7c4
Veysi yê MALA SAF <veysieken000@gmail.com>
2026-09-26 08:32:56 +0300

    feat(com): add clearly marked demo announcement fixture

---
+++ zanistarast-com/promotion-data.js
@@ -0,0 +1,4 @@
+import{PROMOTION_KIND}from"./promotions.js";
+export const promotions=[
+ {id:"demo-announcement-1",version:"demo-1",kind:PROMOTION_KIND.INTERNAL,state:"DEMO",demo:true,title:"Yeni çalışma tanıtımı",summary:"Geliştirme amaçlı tanıtım rafı örneği. Gerçek yayın duyurusu değildir.",targetWorkId:"demo-merhamet",targetVersion:"demo-1",label:"TANITIM"}
+];
